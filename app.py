c8e5f81fe0266f0bf0eb8b74d1080e6cac694a30
Arvind Agarwal <139535632+ArvindAgarwal1310@users.noreply.github.com>
2024-02-25 15:42:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,25 @@ from fastapi import FastAPI
 
 app=FastAPI(title="Testing")
 
-app.get("/")
-def ans():
-    return "Hello"
 
+# initating app
+app = FastAPI(title="Paper Brokerage & Data",redoc_url=None)
+@app.get("/")
+def read_root():
+    return {"message": "Invsto - Paper Brokerage & Data "}
+
+
+# creating origin list
+origins = ['*']
+
+# config CORSM
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+if(__name__=="__app_"):
+    uvicorn.run(app,host="0.0.0.0",port=8080)
